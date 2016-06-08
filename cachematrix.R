a83bab560710d7b9667d33c8e282c6f522027e7e
ranjepeg <ranjeetapegu@gmail.com>
2016-06-08 18:22:21 -0400

    Coursera matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
+##set the value of the matrix
+##get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## x : invertible matrix
+i <- NULL
+set <- function(y){
+## <<- assign a value to an object in an environment different from the current environment
+x <<- y 
+i <- NULL
+}
+get <- function() x
+setinv <- function(inverse)
+i <<- inverse
+getinv <- function() i
+list(set =set , get=get , setinv =setinv, getinv = getinv)
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		i <- x$getinv()
+if(!is.null(i)){
+message("getting cache data")
+return(i)
+}
+mat.data <- x$get()
+i<- solve(mat.data,...)
+x$setinv(i)
+return(i) 
 }
